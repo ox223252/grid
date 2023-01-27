17f59f5928c18000ca2718a55abc69614a06fff0
ox223252 <23252@laposte.net>
2023-01-27 14:11:30 +0100

    add stackGrid

---
+++ StackGrid.js
@@ -0,0 +1,101 @@
+"use strict";
+
+class StackGrid {
+	constructor ( target, options = {} ) {
+		this.target = target;
+		this.options = options;
+
+		this.draw ( );
+
+		window.addEventListener ('resize',()=>{this.draw()});
+	}
+	draw ( force )
+	{
+		this.target.style.display = "flex";
+		this.target.style.alignItems = "flex-start";
+		this.target.style.justifyContent = "space-evenly";
+
+		let width = this.target.getAttribute( "data-grid-width" );
+		let raw = this.target.getElementsByClassName( "grid-raw" );
+		let column = Math.floor ( this.target.clientWidth / width );
+		let divs = this.target.getElementsByClassName( "grid-column" );
+
+		// if not the correct number of column
+		if ( ( divs.length != column ) ||
+			( force = true ) );
+		{
+			// put all data in a div to save normal order
+			// if its not already done
+			if ( raw.length == 0 )
+			{
+				let node = document.createElement( "div" );
+				node.classList.add( "grid-raw" );
+			
+				let els = this.target.getElementsByClassName( "grid-content" );
+
+				for ( let j = 0; els.length > 0; j++ )
+				{
+					els[ 0 ].style.width = "100%";
+					node.appendChild( els[ 0 ] );
+				}
+				this.target.appendChild( node );
+				raw = this.target.getElementsByClassName( "grid-raw" );
+			}
+
+			// clone elements
+			let nodes = raw[ 0 ].cloneNode( true );
+			// hidde backup
+			raw[ 0 ].style.display = "none";
+
+			// get content of clone
+			let els = nodes.getElementsByClassName( "grid-content" );
+
+			// remove olds columns
+			for ( let j = ( divs.length - 1 ); j >= 0; j-- )
+			{
+				this.target.removeChild( divs[ j ] );
+			}
+			
+			// create new columns
+			for ( let j = 0; j < column; j++ )
+			{
+				let node = document.createElement( "div" );
+				node.style.maxWidth = width+"px";
+				node.id = "div_"+j;
+				node.classList.add( "grid-column" );
+				this.target.insertBefore( node, this.target.childNodes[ j ] );
+			}
+
+			// put conned contents in columns
+			for ( let j = 0; els.length > 0; j++ )
+			{
+				let next = 0;
+				for ( let k = 1; k < column; k++ )
+				{
+					if ( this.target.children[ next ].clientHeight > this.target.children[ k ].clientHeight)
+					{
+						next = k;
+					}
+				}
+				this.target.children[ next ].appendChild( els[ 0 ] );
+			}
+		}
+
+		let anchor = location.href.substring(location.href.indexOf("#")+1);
+
+		if ( anchor )
+		{
+			let el = document.getElementById( anchor );
+			if ( el )
+			{
+				window.scrollTo( 0, el.offsetTop );
+			}
+		}
+	}
+	change ( value, grid = this.target )
+	{
+		let width = grid.setAttribute( "data-grid-width", value );
+
+		this.draw ( );
+	}
+}
